36a5a87e86b2f935b814e75fb43f75051d443a94
chang young kim <64997303+kimchang0@users.noreply.github.com>
2024-07-30 20:49:53 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,15 @@
+def solution(n):
+    ans = 0
+    distence = 0
+    now = 0
+    
+    while n > now :
+        if distence < 1 or distence > n:
+            ans += 1
+            distence += 1
+            now += 1
+        else:
+            now += n // (distence * 2)
+            distence *= 2
+            
+    return ans
