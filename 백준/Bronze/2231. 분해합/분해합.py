fafaccaf8cf57c47301ae423faf56ad61d8a5c49
chang young kim <64997303+kimchang0@users.noreply.github.com>
2024-08-29 19:48:36 +0900

    [Bronze II] Title: 분해합, Time: 1248 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+n = int(input().strip())
+
+answer = 1000000
+
+for i in range(n, 0, -1):
+    a = [int(i) for i in str(i)]
+    if i + sum(a) == n:
+        if i <= answer:
+            answer = i
+
+print(str(answer) if answer != 1000000 else str(0))
