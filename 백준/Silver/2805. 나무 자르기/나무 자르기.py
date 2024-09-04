a84fe643609daf803fe411e483957402d1b9737c
chang young kim <64997303+kimchang0@users.noreply.github.com>
2024-09-04 13:47:24 +0900

    [Silver II] Title: 나무 자르기, Time: 4012 ms, Memory: 146412 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+input = sys.stdin.readline
+print = sys.stdout.write
+
+n, m = map(int, input().strip().split())
+trees = list(map(int, input().strip().split()))
+
+start = 0
+end = heapq.nlargest(1, trees)[0]
+result = 0
+
+while start <= end:
+	total = 0
+	mid = (start + end) // 2
+	for i in trees:
+		if i > mid:
+			total += i-mid
+	
+	if total < m:
+		end = mid -1
+  
+	else:
+		result = mid
+		start = mid +1
+  
+print(str(result))
