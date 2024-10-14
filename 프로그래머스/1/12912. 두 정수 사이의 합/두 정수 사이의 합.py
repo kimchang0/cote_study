6fd2182d1eacea9ab18405362de9a5e4a57e836f
chang young kim <64997303+kimchang0@users.noreply.github.com>
2024-10-14 15:50:41 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 1218.06 ms, Memory: 746 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,5 +1,2 @@
 def solution(a, b):
-    if a < b:
-        return sum([i for i in range(a, b + 1)])
-    else:
-        return sum([i for i in range(b, a + 1)])
+    return sum(([i for i in range(a, b - 1 if a > b else b + 1, -1 if a > b else 1)]))
